81a82340eab51a9cf1bf54f348e2f48245184b4e
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-09-18 23:22:52 -0400

    supports two messages, will only print on teacity_build?

--- lib/teamcity_service_messages.rb
+++ lib/teamcity_service_messages.rb
@@ -1,5 +1,19 @@
 require 'teamcity_service_messages/version'
 
-module TeamcityServiceMessages
-  # Your code goes here...
+module TeamCity
+  def teamcity_build?
+    ENV.include?('TEAMCITY_PROJECT_NAME')
+  end
+
+  def teamcity_agent?
+      Dir.exists?('C:/BuildAgent')
+  end
+      
+  def publish_artifacts(path)
+     puts "##teamcity[publishArtifacts '#{path}']" if teamcity_build?
+  end
+
+  def import_data(type, path)
+     puts "##teamcity[importData type='#{type}' path='#{path}']" if teamcity_build?
+  end
 end
