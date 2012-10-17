2b1005e32f40a9c314b991b2753f189962eb9524
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-10-17 12:13:03 -0300

    can't require the stripped down file name/string

--- lib/teamcity_service_messages.rb
+++ lib/teamcity_service_messages.rb
@@ -1,6 +1,6 @@
 lib = File.dirname(File.expand_path(__FILE__))
 Dir.glob(File.join(lib, '/teamcity_service_messages/*.rb')).each do |file|
-  require File.basename(file, File.extname(file))
+  require file
 end
 
 module TeamCityServiceMessages
