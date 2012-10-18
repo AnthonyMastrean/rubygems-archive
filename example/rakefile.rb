46d3a27b62d7fe6f7a3662697fe2f9e01da792e7
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-10-18 14:47:32 -0400

    that's what I get for using git commit -am all the time

--- example/rakefile.rb
+++ example/rakefile.rb
@@ -4,8 +4,8 @@ require 'installshield'
 task :default => [ :msi ]
 
 installshield :msi do |msi|
-  msi.ism = 'd:/code/software/scanner/trunk/install/OmnyxScanner.ism'
-  msi.parameters << "-l PATH_TO_RELEASE_FILES=\"d:\\code\\software\\scanner\\trunk\\bin\\Release\""
+  msi.ism = './install/test.ism'
+  msi.parameters << "-l PATH_TO_RELEASE_FILES=\".\\bin\""
   msi.product_version = '0.0.5.0'
   msi.new_product_code!
 end
