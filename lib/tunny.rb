edb07fbdb8ced27ec03f081dfb325fae0d2b4e70
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-22 17:35:12 -0400

    Update tunny.rb

--- lib/tunny.rb
+++ lib/tunny.rb
@@ -1,26 +1,12 @@
 require_relative "tunny/cli"
+require_relative "tunny/string"
+require_relative "tunny/version"
 
+# add application requires here
 require_relative "tunny/attrib"
 require_relative "tunny/devenv"
 require_relative "tunny/sqlcmd"
 require_relative "tunny/robocopy"
 
-require_relative "tunny/version"
-
 module Tunny
-  
-end
-
-class String
-  def quote
-    "\"#{self}\""
-  end
-  
-  def windows_path
-    gsub "/", "\\"
-  end
-  
-  def posix_path
-    gsub "\\", "/"
-  end
 end
