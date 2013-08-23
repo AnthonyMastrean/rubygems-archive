3b870c8306c7121bc36f6a54b5c77eeb3186fb3d
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-23 14:28:07 -0400

    completely updated the gem name

---
+++ example/rakefile.rb
@@ -0,0 +1,19 @@
+require "slowcheetr"
+
+task :default do
+  puts "pick a task"
+end
+
+desc "test the apprc task"
+apprc :apprc do |app|
+  app.files = FileList["**/app.rc"]
+  app.version = "0.0.5.0"
+end
+
+desc "test the appconfig task"
+appconfig :appconfig do |app|
+  app.files = FileList["**/*.eappe.config"]
+  app.replacements = {
+    "/configuration/applicationSettings/*/setting[@name=\"FullScreen\"]/value" => "True",
+    "/configuration/applicationSettings/*/setting[@name=\"Profile\"]/value" => "Simluation", }
+end
--- lib/slowrcheetah/version.rb
+++
@@ -1,3 +0,0 @@
-module Slowrcheetah
-  VERSION = '0.0.1'
-end
